b3ef8bbdeb7f12850e71ac3fd6bdcbfedddd8b2b
Vasyl Mandik <Vasik_ua@ukr.net>
2025-03-19 23:14:00 -0500

    add navBar styles & header style ferst part

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,83 @@
 import "./Navbar.css";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
+import { useState } from "react";
+import { SiZebpay } from "react-icons/si";
+
+const Menu = () => {
+  return (
+    <>
+      <p>
+        <a href="#home">Home</a>
+      </p>
+      <p>
+        <a href="wGPT3">What GPT3</a>
+      </p>
+      <p>
+        <a href="#possibility">Open AI</a>
+      </p>
+      <p>
+        <a href="#features">Case Studies</a>
+      </p>
+      <p>
+        <a href="#blog">Library</a>
+      </p>
+    </>
+  );
+};
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sing in</p>
+        <button type="button"> Sing up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            size={27}
+            color="#fff"
+            onClick={() => {
+              setToggleMenu(false);
+            }}
+          />
+        ) : (
+          <RiMenu3Line
+            size={27}
+            color="#fff"
+            onClick={() => {
+              setToggleMenu(true);
+            }}
+          />
+        )}
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              <Menu />
+              <div className="gpt3__navbar-menu_container-links-sign">
+                <p>Sing in</p>
+                <button type="button" className="buttonSing">
+                  {" "}
+                  Sing up
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
