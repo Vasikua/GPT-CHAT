6edd5ba42f08fef1999888d623dc5a21c3331085
Vasyl Mandik <Vasik_ua@ukr.net>
2025-03-24 22:55:48 -0500

    feature done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,12 @@ import "./App.css";
 import NavBar from "../Navbar/Navbar";
 import Header from "../../containers/Header/Header";
 import Brand from "../../components/Brand/Brand";
-import Features from "../../containers/Features/Features";
 import Possibility from "../../containers/Possibility/Possibility";
 import WhatGPT3 from "../../containers/WhatGPT3/WhatGPT3";
 import CTA from "../Cta/Cta";
 import Blog from "../../containers/Blog/Blog";
 import Footer from "../../containers/Footer/Footer";
+import Features from "../../containers/Features/Features";
 
 function App() {
   return (
